011ab86ce90ceea62db0a7c5d69fbdc307a7805f
FiveTW87 <thanwalokwattanakul@gmail.com>
2024-01-22 12:53:07 +0700

    feat : complete Ex

--- e-commerce-tables.sql
+++ e-commerce-tables.sql
@@ -1,1 +1,28 @@
 -- Start writing code here
+create table users(
+user_id int primary key generated always as identity,
+username varchar (255) unique not null,
+email varchar (255) unique not null,
+password varchar (255) not null,
+create_at timestamptz unique not null
+);
+
+create table products(
+product_id int primary key generated always as identity,
+name varchar (255) unique not null,
+description text,
+price numeric (10,2) not null,
+create_at timestamptz unique not null
+);
+
+create table categories(
+category_id int primary key generated always as identity,
+name varchar (255) unique not null,
+create_at timestamptz unique not null
+);
+
+create table product_categories(
+product_categories_id int primary key generated always as identity,
+product_id int references products(product_id) on delete cascade,
+category_id int references categories(category_id) on delete cascade
+);
